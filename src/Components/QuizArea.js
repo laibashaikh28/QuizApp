8dd7485d14cb6b618bee850b8c29bf2aa5b53ddb
laiba ovais <laibaovais05@gmail.com>
2020-10-27 21:22:25 +0500

    new

---
+++ src/Components/QuizArea.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Question from './Question'
+import AnswerList from './AnswerList'
+import UserGreeting from './UserGreeting'
+
+function QuizArea(props) {
+    if(props.isFinished){
+        return <UserGreeting />
+    }
+    return (
+        <div>
+            <Question dataSet={props.dataSet}/>
+            <AnswerList handleClick = {props.handleClick} dataSet={props.dataSet}/>
+        </div>
+    )
+}
+
+export default QuizArea
---
+++ src/Components/UserGreeting.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserGreeting() {
+    return (
+        <div>
+            <h2 className="userGreeting">Thank you for completing the questionnaire!</h2>
+        </div>
+    )
+}
+
+export default UserGreeting
